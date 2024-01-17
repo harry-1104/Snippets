f990f104495965f29760eaf4316565553d78e968
Nguyen Quan <132640215+nguyenquan-2510@users.noreply.github.com>
2024-01-17 12:35:13 +0700

    Create Python snippet.py

---
+++ Python snippet.py
@@ -0,0 +1,16 @@
+'''
+	Author: ------ ---- ----   --/--/----
+	Date created: dd/mm/yyyy
+'''
+
+# Functions
+def __FILE(name):
+	try:
+		fi = open(name + ".inp", "r")
+		fo = open(name + ".out", "w")
+		return fi, fo
+	except FileNotFoundError:
+		return None, None
+
+# Start here !
+__FILE()
