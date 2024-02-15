9341dd24cfafb7ae57f1e78bd6fbd129abea37e2
Nguyen Quan <132640215+nguyenquan-2510@users.noreply.github.com>
2024-02-15 19:49:36 +0700

    Update Python snippet.py

--- Python snippet.py
+++ Python snippet.py
@@ -9,7 +9,7 @@ def __FILE(name=None):
 	if name is None: pass
 	else: 
 		try: 
-			open(name+".inp", 'r')
+			open(name + ".inp", 'r')
 			sys.stdin = open(name + ".inp", 'r')
 			sys.stdout = open(name + ".out", 'w')
 			sys.stderr = open(name + ".err", 'w')
