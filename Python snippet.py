673c3504cd81d15af83c31d7d303c23d66ab71f4
Nguyen Quan <132640215+nguyenquan-2510@users.noreply.github.com>
2024-02-15 19:09:18 +0700

    Update Python snippet.py

--- Python snippet.py
+++ Python snippet.py
@@ -5,6 +5,7 @@
 
 import sys
 
+def input(): return sys.stdin.readline()
 def __FILE(name=None):
 	if name is None: pass
 	else: 
@@ -14,9 +15,6 @@ def __FILE(name=None):
 			sys.stdout = open(name + ".out", 'w')
 			sys.stderr = open(name + ".err", 'w')
 		except FileNotFoundError: pass
-
-## This optional function is for reading variables in files
-# def input(): return sys.stdin.readline()
 
 # Start here ! __nguyenquan__
 __FILE("nq")
