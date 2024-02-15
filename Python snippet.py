25828d6b6a02c12eeb0f4210476d7f6d20f4b032
Nguyen Quan <132640215+nguyenquan-2510@users.noreply.github.com>
2024-02-15 18:44:50 +0700

    Update Python snippet.py

--- Python snippet.py
+++ Python snippet.py
@@ -10,6 +10,8 @@ def __FILE(name):
 		sys.stdin = open(name + ".inp", 'r')
 		sys.stdout = open(name + ".out", 'w')
 		sys.stderr = open(name + ".err", 'w')
+
+def input(arg): return sys.stdin.readline(arg)
 # Start here !
 __FILE()
 #
